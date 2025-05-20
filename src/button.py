d9b0c85cbd0db3c781ca6b4355dac4d087881d5a
icetea1110 <s24033@gsm.hs.kr>
2025-05-20 17:48:41 +0900

    버튼 실사용 준비 끝

--- src/button.py
+++ src/button.py
@@ -5,9 +5,10 @@ from entity import Entity
 
 class Button:
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    buttonList = [] # 현재 생성된 버튼 객체가 들어있는 리스트
     
     '''
-        text는 버튼 위에 쓸 text를 입력, color는 평소 색, hilightColor는 마우스가 위에 있을 때의 색, buttonAction은 버튼을 눌렀을 때 실행할 액션
+        text는 버튼 위에 쓸 text를 입력, color는 평소 색, hilightColor는 마우스가 위에 있을 때의 색, buttonAction은 버튼을 눌렀을 때 실행할 함수 혹은 람다식
     '''
     def __init__(self, x, y, width, height, text, color, hilightColor, textColor, buttonAction):
         self.button_color = color
@@ -16,6 +17,7 @@ class Button:
         self.button = pygame.Rect(x, y, width, height)
         self.button_text = text
         self.button_action = buttonAction
+        Button.buttonList.append(self)
     
     '''
         버튼을 그리는 함수
@@ -30,6 +32,12 @@ class Button:
         text_rect = self.text.get_rect(center=self.button.center)
         Button.screen.blit(self.text, text_rect)
     
+    '''
+        버튼 실행을 감지하는 함수
+    '''
     def excuteButton(self, event, *args):
         if event.type == pygame.MOUSEBUTTONDOWN and self.button.collidepoint(event.pos):
             self.button_action(args)
+    
+    def deleteButton(self):
+        Button.buttonList.remove(self)
