3b690be2314168ff2639b7929916824bd32b308a
icetea1110 <s24033@gsm.hs.kr>
2025-06-02 10:31:10 +0900

    study모듈 pygame.gui에 맞춰 수정

--- src/study.py
+++ src/study.py
@@ -1,36 +1,70 @@
 from abc import ABC, abstractmethod
 from statAndStatPoint import Stat
-from button import Button
 from settings import *
+from enum import Enum
+import pygame
 
-class Study(Button, ABC):
-    def __init__(self, price, ability : function, x, y, width, height, text, color, hilightColor, buttonAction : function, textColor=Color.BLACK):
-        super().__init__(x, y, width, height, text, color, hilightColor, buttonAction, textColor)
-        self.ability = ability
-        self.price = price
-    
-    @abstractmethod
-    def study(self):
-        pass
-    
-    def buy(self, prePoint) -> int:
-        # 능력치 수정 후 버튼 색 어둡게 물들이고 text내용을 "품절" 로 바꾸기
-        self.ability()
-        return prePoint - self.price
-    
-    def fail(self):
-        # 스텟 포인트가 부족합니다.
-        pass
+class Subject(Enum):
+    FrontEnd = 0
+    BackEnd = 1
+    PublicCor = 2
+    FunctionClass = 3
 
-class FrontStudy(Study):
-    def __init__(self, price, ability, x, y, width, height, text, color, hilightColor, buttonAction, textColor=Color.BLACK):
-        super().__init__(price, ability, x, y, width, height, text, color, hilightColor, buttonAction, textColor)
+class Study:
+    '''공부 테크 트리를 관리하는 클래스 입니다.'''
+    studyList = {{False, False, False, False, False}, {False, False, False, False, False}, {False, False, False, False, False}, {False, False, False, False, False}}
+    '''이미 찍은 테크 트리인지 아직 찍지 않은 테크 트리인지 알려주는 역할을 합니다. (바깥 리스트는 과목, 내부 리스트는 레벨)'''
     
-    def study(self):
-        if self.price <= Stat.intuitivePoint + Stat.majorSubjectPoint:
-            Stat.intuitivePoint = self.buy(Stat.intuitivePoint)
+    def study(self, subject : Subject, level, price):
+        '''subject는 과목, level은 해당 과목 테크 트리의 레벨 price는 해당 테크를 해제하는데 드는 비용'''
+        if subject == Subject.FrontEnd:
+            self.Front(subject, level, price)
+        elif subject == Subject.BackEnd:
+            self.Back(subject, level, price)
+        elif subject == Subject.PublicCor:
+            self.PublicCor(subject, level, price)
+        elif subject == Subject.FunctionClass:
+            self.Function(subject, level, price)
+    
+    def Front(self, subject : Subject, level, price):
+        if price <= Stat.intuitivePoint + Stat.majorSubjectPoint:
+            Stat.intuitivePoint = Stat.intuitivePoint - price
             if Stat.intuitivePoint < 0:
                 Stat.majorSubjectPoint += Stat.intuitivePoint
                 Stat.intuitivePoint = 0
+            
+            Study.studyList[subject][level] = True
+            
         else:
             self.fail()
+    
+    def Back(self, subject : Subject, level, price):
+        if price <= Stat.interpretPoint + Stat.majorSubjectPoint:
+            Stat.interpretPoint = Stat.interpretPoint - price
+            if Stat.interpretPoint < 0:
+                Stat.majorSubjectPoint += Stat.interpretPoint
+                Stat.interpretPoint = 0
+            
+            Study.studyList[subject][level] = True
+        else:
+            self.fail()
+    
+    def PublicCor(self, subject : Subject, level, price):
+        if price <= Stat.normalSubjectPoint:
+            Stat.normalSubjectPoint = Stat.normalSubjectPoint - price
+            
+            Study.studyList[subject][level] = True
+        else:
+            self.fail()
+    
+    def Function(self, subject : Subject, level, price):
+        if price <= Stat.normalSubjectPoint:
+            Stat.normalSubjectPoint = Stat.normalSubjectPoint - price
+            
+            Study.studyList[subject][level] = True
+        else:
+            self.fail()
+    
+    def fail(self):
+        # 스탯 포인트가 부족합니다.
+        pass
