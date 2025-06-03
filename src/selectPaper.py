76b9467880824709528f80bba16af347194ceb39
icetea1110 <s24033@gsm.hs.kr>
2025-06-03 19:01:47 +0900

    UI수정 테스트 및 수정 완료

--- src/selectPaper.py
+++ src/selectPaper.py
@@ -3,15 +3,14 @@ import pygame_gui
 from pygame.locals import QUIT
 from settings import *  # 설정 값 가져오기
 from entity import Entity
-from game import Game
 
 class SelectPaper:
     def __init__(self, imagePath, title, text, manager : pygame_gui.UIManager, *buttons : pygame_gui.elements.UIButton):
         '''
             title은 이벤트 제목(html태크 없이 순수 문자열로), text는 이벤트 설명, uiManager는 UI를 draw하던 UIManager를 받는다., buttons는 이벤트의 선택지들을 설정, condition은 선택지가 뜨는 조건, imagePath는 이벤트 상황을 표현하는 그림의 경로
         '''
-        self.x = SCREEN_WIDTH / 2
-        self.y = SCREEN_HEIGHT / 2
+        self.x = SCREEN_WIDTH / 8
+        self.y = SCREEN_HEIGHT / 10
         self.width = SCREEN_WIDTH / 1.3
         self.height = SCREEN_HEIGHT / 1.3
         image_rect = pygame.image.load(imagePath).convert_alpha()
@@ -24,24 +23,24 @@ class SelectPaper:
         )
         
         self.image = pygame_gui.elements.UIImage(
-            relative_rect=pygame.Rect((self.x, self.y / 2), (self.width / 1.1, self.height / 3)),
+            relative_rect=pygame.Rect((self.x / 4, self.y / 4), (self.width / 1.1, self.height / 3)),
             image_surface=image_rect,
             manager=manager,
             container=self.panel,
             object_id="#select_paper_image"
         )
         
-        self.title = pygame_gui.elements.UITextBox(
-            html_text=f"<h1>{title}</h1>",
-            relative_rect=pygame.Rect((self.x, self.y), (self.width, self.width / 10)),
+        self.title = pygame_gui.elements.UILabel(
+            relative_rect=pygame.Rect((-3, self.y * 3), (self.width, self.width / 10)),
+            text=title,
             manager=manager,
             container=self.panel,
             object_id="#select_paper_title"
         )
         
-        self.text = pygame_gui.elements.UITextBox(
-            html_text=text,
-            relative_rect=pygame.Rect((self.x, self.y + 50 + (self.height / 4 - 50)), (self.width, self.height / 2 - 50)),
+        self.text = pygame_gui.elements.UILabel(
+            relative_rect=pygame.Rect((-3, self.y * 4), (self.width, self.height / 2 - 50)),
+            text=text,
             manager=manager,
             container=self.panel,
             object_id="#select_paper_text"
@@ -49,7 +48,6 @@ class SelectPaper:
         
         self.manager = manager
         self.buttons = buttons
-        self.gameInstance = Game()
     
     def kill(self):
         self.panel.kill()
