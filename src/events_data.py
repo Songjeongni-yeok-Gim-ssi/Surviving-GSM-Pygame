0c1615f830de1774fc040097add487d2d3d7d428
Dino0204 <s24020@gsm.hs.kr>
2025-06-16 08:23:13 +0900

    fix: 자격증 시험 이벤트의 확률 값을 0.1에서 0.02로 수정(현재의 1/5로 감소)

--- src/events_data.py
+++ src/events_data.py
@@ -520,7 +520,7 @@ def get_events_data():
                 'title': '자격증 시험',
                 'text': '자격증 시험을 볼 기회가 생겼습니다.',
                 'image': 'assets/imgs/events/certification_exam.png',
-                'probability': 0.1,
+                'probability': 0.02,
                 'repeatable': True,
                 'time_trigger': {
                     'week': [1, 30],
